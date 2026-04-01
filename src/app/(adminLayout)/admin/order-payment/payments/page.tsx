c63fbcdfcfcd61dd14a7bb02406bcc12e61d8ca5
Shimu Ziasmin <143342894+Shimu2621@users.noreply.github.com>
2026-03-31 20:54:17 -0400

    Create payment page

---
+++ src/app/(adminLayout)/admin/order-payment/payments/page.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { useState } from "react";
+import { useGetAllPaymentsQuery } from "@/redux/api/payment/paymentApi";
+import PaymentStats from "@/components/dashboard/payment/PaymentStats";
+import PaymentTable from "@/components/dashboard/payment/PaymentTable";
+
+const PaymentPage = () => {
+  const { data, isLoading } = useGetAllPaymentsQuery();
+
+  const [page, setPage] = useState(1);
+  const limit = 5;
+
+  const payments = data?.data || [];
+
+  const paginatedData = payments.slice((page - 1) * limit, page * limit);
+
+  return (
+    <div className="p-6 space-y-6">
+      <h1 className="text-3xl font-bold text-blue-600">Payment Management</h1>
+
+      <PaymentStats payments={payments} />
+
+      <PaymentTable
+        payments={paginatedData}
+        total={payments.length}
+        page={page}
+        setPage={setPage}
+        limit={limit}
+        isLoading={isLoading}
+      />
+    </div>
+  );
+};
+
+export default PaymentPage;
