477b987a9b836a2fbd09706cab64ad5e9f4be67d
Shimu Ziasmin <143342894+Shimu2621@users.noreply.github.com>
2026-04-01 19:23:29 -0400

    update files

--- src/types/payment/payment.ts
+++ src/types/payment/payment.ts
@@ -1,10 +1,13 @@
+import { IOrder } from "../order/order";
+import { IUser } from "../user/user";
+
 export type PaymentStatus = "pending" | "paid" | "cancelled";
 export type PaymentMethodType = "pay_now" | "cod";
 
 export interface IPayment {
   _id: string;
-  userId: string;
-  orderId: string;
+  userId: IUser; // ✅ FIXED
+  orderId: IOrder; // ✅ FIXED
   amount: number;
   paymentMethod: PaymentMethodType;
   paymentStatus: PaymentStatus;
