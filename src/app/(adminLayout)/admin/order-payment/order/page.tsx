0209da4ab30b5003d9ca24be373f789a30b1f051
Shimu Ziasmin <143342894+Shimu2621@users.noreply.github.com>
2026-04-08 23:43:38 -0400

    update file

--- src/app/(adminLayout)/admin/order-payment/order/page.tsx
+++ src/app/(adminLayout)/admin/order-payment/order/page.tsx
@@ -6,6 +6,7 @@ import { useGetAllOrdersQuery } from "@/redux/api/order/orderApi";
 import OrderStats from "@/components/admin/order/OrderStats";
 import OrderTable from "@/components/admin/order/OrderTable";
 import { ListOrdered } from "lucide-react";
+import { AuroraText } from "@/components/magicui/aurora-text";
 
 const OrderPage = () => {
   const { data: orders = [], isLoading } = useGetAllOrdersQuery();
@@ -29,6 +30,27 @@ const OrderPage = () => {
         <p className="text-muted-foreground">
           Manage and track customer orders efficiently by updating status, and
           ensuring smooth order delivery
+        </p>
+      </motion.div>
+
+      {/* header */}
+      <motion.div
+        initial={{ opacity: 0, y: 10 }}
+        animate={{ opacity: 1, y: 0 }}
+        className="text-center space-y-2"
+      >
+        <div className="flex items-center justify-center space-x-3">
+          <div className="p-2 rounded-full bg-gradient-to-r from-rose-500 via-purple-500 to-blue-500">
+            <ListOrdered className="h-8 w-8 text-white" />
+          </div>
+
+          <AuroraText className="text-3xl md:text-4xl font-bold">
+            Category Management
+          </AuroraText>
+        </div>
+        <p className="text-muted-foreground">
+          Structure your product catalog with categories and subcategories to
+          improve organizations, and overall shopping experiences
         </p>
       </motion.div>
 
