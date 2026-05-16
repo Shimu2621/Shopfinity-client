63786b1860fda4458922bead3285950e108b6a1a
Shimu Ziasmin <143342894+Shimu2621@users.noreply.github.com>
2026-05-16 19:49:43 -0400

    update file

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -106,6 +106,7 @@ export const {
   useGetAllPaymentsQuery,
   useGetPaymentByIdQuery,
   useCreatePaymentMutation,
+  useUpdatePaymentMutation,
   useCreateStripeSessionMutation,
   usePaymentSuccessMutation,
   usePaymentCancelMutation,
